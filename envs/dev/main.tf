170fa22006e003b9e2429d066b2fbd658fd26055
mohamed1311990 <mohamed.salem.say@gmail.com>
2026-02-18 09:22:50 +0100

    eks

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -6,6 +6,27 @@ module "vpc" {
   env    = var.env
 }
 
+ resource "aws_iam_role" "admin_role" {
+   name = "admin-role"
+   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        AWS = var.trusted_principal_arn
+      }
+      Action = "sts:AssumeRole"
+    }]
+   })
+  }
+ 
+ resource "aws_iam_role_policy_attachment" "admin_eks_full_access" {
+  role       = aws_iam_role.admin_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ }
+
+
+
 module "eks" {
   source = "../../modules/eks"
 
@@ -13,7 +34,7 @@ module "eks" {
   env        = var.env
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-  
-  admin_principal_arn = var.admin_principal_arn
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
+  admin_role_principal_arn = aws_iam_role.admin_role.arn
+
 }
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,5 +1,9 @@
 variable "region" { type = string }
 variable "name"   { type = string }
 variable "env"    { type = string }
-variable "admin_principal_arn" {type = string}
 variable "cluster_endpoint_public_access_cidrs" {type = list(string)}
+variable "trusted_principal_arn" {
+  description = "IAM user or role ARN allowed to assume this role"
+  type        = string
+}
+
